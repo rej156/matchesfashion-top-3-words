b92cbe90f18a6c69958b91633f6b2bbc2bf7ed30
Eric Juta <ericjohn.juta@consensys.net>
2020-01-25 19:26:02 +0000

    Sanitize input and refactor

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,32 @@
+const sanitizeInput = (input: string): string[] => {
+  const matched = input.toLowerCase().match(/[^\s.,;!?/]+/g);
+  return matched ? matched : [];
+};
+
+const recurringWordsByCount = (sanitizedWords: string[]): { [word: string]: number } =>
+  sanitizedWords.reduce((current: { [key: string]: number }, next: string) => {
+    if (typeof next === "string") {
+      if (current[next]) {
+        current[next] += 1;
+      } else {
+        current[next] = 1;
+      }
+      return current;
+    }
+    return current;
+  }, {});
+
+const sortWordsByCount = (sortedWords: { [word: string]: number }): string[] =>
+  Object.keys(sortedWords).sort((current, next) => sortedWords[next] - sortedWords[current]);
+
 const top3Words = (input: string): Array<string> => {
   // TypeScript is unsound :(
   if (typeof input === "string") {
-    // TODO sanitize
-    //
-    const recurringWordsByCountObject = input.split(" ")
-      .map(word => word.toLowerCase())
-      .reduce((current: { [key: string]: number }, next: string) => {
-      if (typeof next === "string") {
-        if (current[next]) {
-          current[next] += 1;
-        } else {
-          current[next] = 1;
-        }
-        return current;
-      }
-      return current;
-    }, {});
-    return Object.keys(recurringWordsByCountObject).sort(function(a, b) {
-      return recurringWordsByCountObject[b] - recurringWordsByCountObject[a];
-    }).slice(0, 3);
-    // return [];
+    const sanitizedInput = sanitizeInput(input);
+    const recurringWordsByCountObject = recurringWordsByCount(sanitizedInput);
+    const sortedWordsByCount = sortWordsByCount(recurringWordsByCountObject);
+
+    return sortedWordsByCount.slice(0, 3);
   } else {
     throw new Error("Please input a string to process and return the top 3 words!");
   }
